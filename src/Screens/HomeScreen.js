2673541ed5e9371c165d577fe57cd7f423dd5724
occiandiaali <ocean.diaali@gmail.com>
2020-07-18 13:45:09 +0100

    change hard-coded alt value to templated

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -11,7 +11,7 @@ function HomeScreen (props) {
                 <li>
                   <div className="product">
                     <Link to={`/product/${product._id}`}> 
-                    <img className="product-image" src={product.image} alt="product" />
+                    <img className="product-image" src={product.image} alt={product.name} />
                     </Link> 
                     <div className="product-name">
                       <Link to={`/product/${product._id}`}>{product.name}</Link>
